7e2c0c2eb33bd3e07a232e4e6163f7f6dcb56a93
rajkiran <rajkiran_ch@hotmail.com>
2015-07-02 17:56:35 -0600

    Basic Structure Creation

--- SysproETLDesktopUI/Program.cs
+++ SysproETLDesktopUI/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SysPro.ETL
+namespace SysproETLDesktopUI
 {
     static class Program
     {
@@ -16,8 +16,7 @@ namespace SysPro.ETL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
-
+            Application.Run(new Form1());
         }
     }
 }
